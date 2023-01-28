5768918cba7bf6102af715f12db315c67e233530
RajshreeJaju <111057499+RajshreeJaju@users.noreply.github.com>
2023-01-28 23:07:31 +0530

    Create addition_fun

---
+++ addition_fun.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void addition(){
+    float a,b;
+    printf("Please enter two numbers for performing addition\nEnter the first number for addition:");
+    scanf("%f",&a);
+    printf("Enter the second number for addition:");
+    scanf("%f",&b);
+    printf("The sum of given numbers is: %.3f",a+b);
+    
+}
+int main(){
+    addition();
+    return 0;
+    
+} 
