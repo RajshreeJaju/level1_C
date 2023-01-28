c34ddf74208b90153f7ec4e2e4fb40ab92912d6c
RajshreeJaju <111057499+RajshreeJaju@users.noreply.github.com>
2023-01-28 22:27:59 +0530

    Update addition

--- addition.c
+++ addition.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main(){
     float a,b;
-    printf("Please a enter two number for performing addition\nEnter first number for addition:");
+    printf("Please enter two number for performing addition\nEnter first number for addition:");
     scanf("%f",&a);
     printf("Enter second number for addition:");
     scanf("%f",&b);
