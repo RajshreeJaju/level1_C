8c1b4aca99d8aaa0cb847ea3ae219ec67f89e994
RajshreeJaju <111057499+RajshreeJaju@users.noreply.github.com>
2023-01-29 10:45:13 +0530

    addition using function commited

--- addition_fun.c
+++ addition_fun.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 void addition(){
-    float a,b;
+    float a,b,c;
     printf("Please enter two numbers for performing addition\nEnter the first number for addition:");
     scanf("%f",&a);
     printf("Enter the second number for addition:");
     scanf("%f",&b);
-    printf("The sum of given numbers is: %.3f",a+b);
+    c=a+b;
+    printf("The sum of given numbers is: %.3f",c);
     
 }
 int main(){
