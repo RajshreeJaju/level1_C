26834a16521ff5b519ff62b091ff9693d53d5316
RajshreeJaju <111057499+RajshreeJaju@users.noreply.github.com>
2023-01-30 11:38:49 +0530

    addition using functions committed

--- addition_fun.c
+++ addition_fun.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
-void addition(){
-    float a,b,c;
-    printf("Please enter two numbers for performing addition\nEnter the first number for addition:");
+#include <stdio.h>
+
+float add(float x,float y){
+   
+    return(x+y);
+}
+int main()
+{
+    float add(float x,float y);
+    float sum,a,b;
+    printf("Enter two numbers to perform addition.\nEnter the first number for addition:");
     scanf("%f",&a);
     printf("Enter the second number for addition:");
-    scanf("%f",&b);
-    c=a+b;
-    printf("The sum of given numbers is: %.3f",c);
-    
-}
-int main(){
-    addition();
+    scanf("%f",&b); 
+    sum=add(a,b);
+    printf("Addition of the two numbers is: %.3f",sum); 
+
     return 0;
-    
 } 
