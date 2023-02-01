f58d6f0903b1f6514eb7cda5f48a45d448638f11
RajshreeJaju <111057499+RajshreeJaju@users.noreply.github.com>
2023-02-01 17:59:06 +0530

    printf() statement separated commit

--- addition.c
+++ addition.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 void main(){
     float a,b;
-    printf("Please enter two numbers for performing addition\nEnter the first number for addition:");
+    printf("Enter two numbers for performing addition\n");
+    printf("Enter the first number for addition:");
     scanf("%f",&a);
     printf("Enter the second number for addition:");
     scanf("%f",&b);
     printf("The sum of given numbers is: %.3f",a+b);
-} 
+}  
 
